afc62cf841cb866cf7db104705bbb0f30d65c583
Uzair Leo <52172428+uzairleo@users.noreply.github.com>
2019-10-26 17:07:16 +0500

    adding Guess range too

--- NumberGameinjavaUingRandomnumber/src/oop/java/Main.java
+++ NumberGameinjavaUingRandomnumber/src/oop/java/Main.java
@@ -16,7 +16,7 @@ public  class  Main{
             System.out.print("*");
         }
         System.out.println("");
-    }
+    }//starLine
 
     static void menu()
    {
@@ -25,13 +25,13 @@ public  class  Main{
        starLine(42);
        System.out.println("(a)Normal \t\t (b)Medium\t\t(c)Hard");
        starLine(42);
-   }
+   }//menu
    static int randNum(int gRange)
 
    {
        int rNumber=(int)((Math.random()*gRange)+1);
        return rNumber;
-   }
+   }//randNum
    static void setLevelOfGame(int guessLimit,int guessRange){
        int randNumber=randNum(guessRange);
        boolean hasWon=false;
@@ -75,7 +75,7 @@ public  class  Main{
            System.out.println("Select Normal level to increase ur chances to win :)");
            starLine(50);
        }
-   }
+   }//setGameLevel
 
     public static void main(String leo[])
     {
@@ -108,182 +108,6 @@ public  class  Main{
        } while (option=='y');
 
 
-    }
-}
-//class Main
-//    {
-//        public static void main(String uzairleo[]) {
-//
-//
-//        int randnumber = (int) ((Math.random() * 100) + 1);
-//        boolean haswongame = false;
-//        int guessnumber;
-//        char choice='n';
-//        do {
-//            System.out.println("This is Number Game u have to guess a number btw 1to100");
-//            Scanner input = new Scanner(System.in);
-//            for (int i = 10; i > 0; i--) {
-//                System.out.println("you have" + i + " guesses left " + "\n");
-//
-//                guessnumber = input.nextInt();
-//                if (randnumber > guessnumber) {
-//                    System.out.println("Its greater than " + guessnumber);
-//                }
-//                if (randnumber < guessnumber) {
-//                    System.out.println("Its less than " + guessnumber );
-//
-//                }
-//                if (randnumber == guessnumber) {
-//                    haswongame = true;
-//                    break;
-//                }
-//            }//loop+
-//            if (haswongame) {
-//                System.out.println("Congrat u won the game");
-//                System.out.println("your guess number is finally equal to random number ==" + randnumber + "\n");
-//
-//            } else {
-//                System.out.println("Sorry you lose the game ");
-//
-//            }
-//
-//            System.out.println("If u want to play Again press y other wise press N to terminate");
-//            choice=input.next().charAt(0);
-//        }while (choice=='y');
-//    }
-//}
+    }//main
+}//class
 
-//public class Main{
-//    public static void main(String[] args){
-//        int randno=(int )(Math.random()*100)+1;
-//        int uuserno=0;
-//        Scanner input=new Scanner(System.in);
-//        int i=5;
-//        do {
-//            System.out.println("plz guess a number btw 1-100");
-//            uuserno=input.nextInt();
-//            if(randno<uuserno) {
-//               System.out.println("The random number is smaller than "+ uuserno);
-//            }if (randno>uuserno) {
-//            System.out.println("The random number is larger than "+uuserno);
-//            }if (randno==uuserno)
-//            {
-//
-//                System.out.println("Hurrah u find that\nrandom no equal to guess number");
-//              System.out.println("random no:"+randno+" \tusernumber :"+uuserno);
-//
-//            }
-//        }while(randno!=uuserno);
-//    }
-//}
-
-
-
-//package oop.java;
-//import java.util.Scanner;
-//class Main{
-//    public static void main(String args[]){
-//
-//        int arr[]={11,22,30,99,100};
-//
-//        Scanner input=new Scanner( System.in);
-//
-//        int item=input.nextInt();
-//        int loc;
-//        int BEG = 0;
-//        int END= 4;
-//        int MID=BEG+END/2;
-//        while ((BEG<=END)&&(arr[MID]!=item)){
-//            if (item<arr[MID]){
-//                END=MID-1;
-//
-//            }else
-//            {
-//                BEG=MID+1;
-//            }
-//            MID=(BEG+END)/2;
-//        }
-//        if (arr[MID]==item){
-//            loc=MID;
-//
-//        }else{
-//            loc=0;
-//        }
-//        System.out.println("The item is present on location === "+loc);
-//    }
-//}
-//
-//
-////package  oop.java;
-////import java.util.Scanner;
-////
-////
-////class  Main{
-////    public static  void main(String arg[])
-////    {
-////        char opt='y';
-////        Scanner input=new Scanner(System.in);
-////        int x=0;
-////        int sum=0;
-////        while(x>=0){
-////            System.out.println("Enter a number u want to calculate its sum factorial ");
-////            x=input.nextInt();
-////            sum=sum+x;
-//////            System.out.println("if u want to continue press y/n");
-//////            opt=input.next().charAt(0);
-////
-////            System.out.println("The Result is == "+sum);
-////
-////
-////        }
-////    }
-////
-////}
-////
-////
-//////package oop.java;
-//////
-//////import java.io.File;
-//////import java.io.FileNotFoundException;
-//////import java.util.Scanner;
-//////import java.lang.String;
-//////public class Main {
-//////    static void openFile(String s)throws Exception
-//////    {
-//////        File file= new File("somefile.txt");
-//////    }
-//////
-//////    public static void main(String[] args) throws Exception{
-//////
-//////        float a,b, result;
-//////        char option;
-//////        Scanner cin=new Scanner(System.in);
-//////        do {
-//////            a=cin.nextInt();
-//////            b=cin.nextInt();
-//////            if (b==0)
-//////            {
-//////                try {
-//////                    float c = a / b;
-//////                }
-//////                catch (ArithmeticException e)
-//////                {
-//////                    System.out.println(e);
-//////                }
-//////            }else
-//////            {
-//////                result=a/b;
-//////                System.out.println("The Division of two number is == "+result);
-//////            }if(b==0){System.out.println("Sorry divide by zero not allowed");}
-//////            System.out.println("If u want to continue division press y/N");
-//////            option=cin.next().charAt(0);
-//////            try {
-//////                openFile("somefile.txt");
-//////            }catch (FileNotFoundException e)
-//////             {
-//////                System.out.println("File not found sorry");
-//////            }
-//////        }while (option=='y');
-//////        }
-//////
-//////}
